d2ec7df358a8d254bb96351e0934f23398339e16
RafaFerreira07 <95254601+RafaFerreira07@users.noreply.github.com>
2022-01-07 20:41:07 -0300

    ProjetoElevador

---
+++ ProjetoElevador/Models/Elevador.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace ProjetoElevador.Models
+{
+    internal class Elevador
+    {
+        /*Inicialização do elevador no terreo!*/
+        public static void inicializar(int terreo)
+        {
+            int andar_atual = terreo;
+            Console.WriteLine("\nElevador iniciado!");
+        }
+
+        /*Definição de Andares do prédio*/
+        public static int andares()
+        {
+            Console.WriteLine("Insira a quantidade de andares do prédio: ");
+            int andares = int.Parse(Console.ReadLine());
+            return andares;
+        }
+
+        /*Definição da capacidade de pessoas dentro do elevador*/
+        public static int capacidade()
+        {
+            Console.WriteLine("Insira a capacidade do elevador: ");
+            int capacidade = int.Parse(Console.ReadLine());
+            return capacidade;
+        }
+
+        /*Definição de pessoas que ja estão dentro do elevador*/
+        public static int pessoas(int capacidade)
+        {
+            Console.WriteLine("Quantas pessoas estão no elevador?");
+            int pessoas = int.Parse(Console.ReadLine());
+            if (pessoas > capacidade)
+            {
+                while (pessoas > capacidade)
+                {
+                    Console.WriteLine("A quantidade de pessoas excede o limite de capacidade!");
+                    Console.WriteLine("Quantas pessoas estão no elevador?");
+                    pessoas = int.Parse(Console.ReadLine());
+                }
+            }
+            return pessoas;
+        }
+
+        /*Função para adicionar uma pessoa ao elevador*/
+        public static int entrar(int pessoas, int capacidade)
+        {
+            if (pessoas >= capacidade)
+            {
+                Console.WriteLine("Não é possivel entrar no elevador!");
+                return pessoas;
+            }
+            else
+            {
+                return pessoas + 1;
+            }
+        }
+
+        /*Função para retirar uma pessoa do elevador*/
+        public static int sair(int pessoas)
+        {
+            if (pessoas == 0)
+            {
+                Console.WriteLine("Não há ninguem no elevador!");
+                return pessoas;
+            }
+            else
+            {
+                return pessoas - 1;
+            }
+        }
+
+        /*Função para subir um andar*/
+        public static int subir(int andar_atual, int andares)
+        {
+            if (andar_atual == andares)
+            {
+                Console.WriteLine("Ñâo é possivel subir pois chegamos ao topo!");
+                return andar_atual;
+            }
+            else
+            {
+                return andar_atual + 1;
+            }
+        }
+
+        /*Função para descer um andar*/
+        public static int descer(int andar_atual, int terreo)
+        {
+            if (andar_atual == 0)
+            {
+                Console.WriteLine("Não é possivel descer pois estamos no térreo!");
+                return andar_atual;
+            }
+            else
+            {
+                return andar_atual - 1;
+            }
+
+        }
+        
+        /*Funcção para decisão da próxima ação do elevador*/
+        public static int run(int answer)
+        {
+            Console.WriteLine(
+                        "\n1 - Subir um andar\n" +
+                        "2 - Descer um andar\n" +
+                        "3 - Adicionar uma pessoa ao elevador\n" +
+                        "4 - Remover uma pessoa do elevador\n" +
+                        "5 - Parar elevador e sair\n");
+            var x = Console.ReadLine();
+            if(x == "")
+            {
+                answer = 0;
+                return answer;
+            }
+            else
+            {
+
+                answer = int.Parse(x);
+                return answer;
+            }
+        }
+    }
+}
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using ProjetoElevador.Models;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Roda()
+            {
+                static int Run()
+                {
+                    int andares = Elevador.andares();
+                    int capacidade = Elevador.capacidade();
+                    int pessoas = Elevador.pessoas(capacidade);
+                    Elevador.inicializar(0);
+                    int andar_atual = 0;
+
+                    Console.WriteLine(
+                            "\n1 - Subir um andar\n" +
+                            "2 - Descer um andar\n" +
+                            "3 - Adicionar uma pessoa ao elevador\n" +
+                            "4 - Remover uma pessoa do elevador\n" +
+                            "5 - Parar elevador e sair\n");
+                    int answer = int.Parse(Console.ReadLine());
+
+                    while (answer != 5)
+                    {
+                        if (answer == 1)
+                        {
+                            andar_atual = Elevador.subir(andar_atual, andares);
+                            Console.WriteLine($"Andar atual: {andar_atual}");
+                            answer = Elevador.run(answer);
+                        }
+                        else if (answer == 2)
+                        {
+                            andar_atual = Elevador.descer(andar_atual, andares);
+                            Console.WriteLine($"Andar atual: {andar_atual}");
+                            answer = Elevador.run(answer);
+                        }
+                        else if (answer == 3)
+                        {
+                            pessoas = Elevador.entrar(pessoas, capacidade);
+                            Console.WriteLine($"Pessoas no elevador: {pessoas}");
+                            answer = Elevador.run(answer);
+                        }
+                        else if (answer == 4)
+                        {
+                            pessoas = Elevador.sair(pessoas);
+                            Console.WriteLine($"Pessoas no elevador: {pessoas}");
+                            answer = Elevador.run(answer);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Selecione uma opção válida!");
+                            answer = Elevador.run(answer);
+                        }
+                    }
+                    Console.WriteLine("\nDeseja usar o elevador novamente?\n1 - Sim\n2 - Não\n");
+                    var nov = Console.ReadLine().ToLower();
+                    while (nov == "")
+                    {
+                        Console.WriteLine("Digite o número da opção que deseja!");
+                        Console.WriteLine("Deseja usar o elevador novamente?\n 1 - Sim\n 2 - Não\n");
+                        nov = Console.ReadLine().ToLower();
+                    }
+                    int response = int.Parse(nov);
+                    return response;
+                }
+                int response = Run();
+
+                while (response == 1)
+                {
+                    Elevador.inicializar(0);
+                    response = Run();
+                }
+            }
+            Roda();
+        }
+    }
+}
